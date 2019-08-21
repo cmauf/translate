fcf9c1058d8531ed6df3d7742070efcbef16c01c
ncay <ncay@192.168.0.10>
2019-08-21 21:23:50 +0200

    working version 1.0

--- translate.java
+++ translate.java
@@ -1,4 +1,3 @@
-package source;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -72,6 +71,11 @@ public class translate {
 	        return current.isEndOfWord();
 	    }
 	    
+	    /** Method added by @cmauf, returns the translation of a given String. 
+	     * Before calling, check if given String exists in Trie.
+	     * @param Word that is looked for
+	     * @return translation 
+	     */
 	    String getTranslation(String word) {
 	        TrieNode current = root;
 
@@ -110,10 +114,16 @@ public class translate {
 	    }
 	}
 	
-	private static Pattern dictline = Pattern.compile("([a-z]+):([a-z]+)");
+	/** Regular expression to make sure dictionary input is formatted correctly
+	 * 
+	 */
+	private static Pattern dictline = Pattern.compile("([a-z]+):([a-z]+)"); 
 
+	/** Main Method, takes dictionary and input streamt to translate
+	 * @param args source of dictionary file
+	 */
 	public static void main(String[] args) {
-		try {
+		try {//check if called correctly
 			String a = (args[0]);
 			a.charAt(1);
 		} 
@@ -123,10 +133,10 @@ public class translate {
 		}
 		Trie Dictionary = new Trie ();
 		try {
-			Scanner scanner = new Scanner(new File(args[0]));
-			while (scanner.hasNextLine()) {
-				String T  = scanner.nextLine();
-				Matcher m = dictline.matcher(T);
+			Scanner scanner = new Scanner(new File(args[0])); //read input file for dictionary
+			while (scanner.hasNextLine()) { //read line for line
+				String T  = scanner.nextLine(); 
+				Matcher m = dictline.matcher(T); //check for conformity with format constraints
 				if (m.matches()) {
 					Dictionary.insert(m.group(1),m.group(2));
 				}
@@ -142,22 +152,47 @@ public class translate {
 		}
 		Scanner input = new Scanner(System.in);
 		while (input.hasNextLine()){
-			String line = input.nextLine();
-			String [] words = line.split(" ");
-			for (String str:words) { //TODO: handle uppercase words correctly, handle double whitespace correctly
-				if (str.contains("-")) {
+			String line = input.nextLine();//read input stream line by line
+			String [] words = line.split(" ");// split line in words
+			for (String str:words) {
+				boolean punct = false;
+				char end = 0;
+				if (str.length()>0 &&!Character.isLetterOrDigit(str.charAt(str.length()-1))) { //split string if ends in punctuation
+					end = str.charAt(str.length()-1);
+					str = str.substring(0,str.length()-1);
+					punct = true;
+				}
+				if ((str.length() == 1 && (Character.isWhitespace(str.charAt(0)) || !Character.isLetterOrDigit(str.charAt(0)))) || str.length()==0) System.out.print(str); //don't do anything for whitespace or empty lines
+				else if (str.contains("-")) { //split compund words
 					String [] parts = str.split("-");
-					//TODO: handle compound words
+					for (int i=0; i< parts.length; i++) {
+						boolean uppercase = Character.isUpperCase(parts[i].charAt(0)); //check if upper case
+						if(Dictionary.containsNode(parts[i].toLowerCase())) {
+							String T = Dictionary.getTranslation(parts[i].toLowerCase());
+							if (uppercase) System.out.print(T.substring(0,1).toUpperCase() + T.substring(1));
+							else System.out.print(T);
+						}
+						else {
+							System.out.print("<" + parts[i] + ">");
+						}
+						if (i!=parts.length-1) System.out.print("-");
+					}
 				}
 				else {
+					boolean uppercase = Character.isUpperCase(str.charAt(0));
 					if(Dictionary.containsNode(str.toLowerCase())) {
-						System.out.print(Dictionary.getTranslation(str.toLowerCase())+" ");
+						String T = Dictionary.getTranslation(str.toLowerCase());
+						if (uppercase) System.out.print(T.substring(0,1).toUpperCase() + T.substring(1));
+						else System.out.print(T);
 					}
 					else {
 						System.out.print("<" + str + ">");
 					}
 				}
+				if (punct) System.out.print(end);
+				if (!words[words.length-1].equals(str+end)) System.out.print(" ");
 			}
+			
 			System.out.println();
 		}
 		input.close();
